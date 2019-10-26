582b62a0215c9aecb484479d96665433d15a1aba
awesomelemonade <awesomelemonade@gmail.com>
2019-10-26 17:11:06 -0400

    Form progress

--- index.js
+++ index.js
@@ -6,19 +6,47 @@ function applyUncheckedStyling(radio) {
 	radio.parentNode.classList.remove("active");
 }
 
-function onRadio() {
-	var radioButtons = document.getElementsByName("tipradio");
+function applyToRadioButtons(func) {
+	var radioButtons = document.getElementsByClassName("fancy-radio");
 	for (let value of radioButtons) {
-		if (value.checked) {
-			applyCheckedStyling(value);
-		} else {
-			applyUncheckedStyling(value);
+		var x = value.getElementsByTagName("input");
+		for (let y of x) {
+			func(y);
 		}
 	}
 }
 
-var radioButtons = document.getElementsByName("tipradio");
-for (let value of radioButtons) {
-	value.setAttribute("onchange", "onRadio()");
+function onRadio() {
+	applyToRadioButtons(x => {
+		if (x.checked) {
+			applyCheckedStyling(x);
+		} else {
+			applyUncheckedStyling(x);
+		}
+	});
 }
+
+applyToRadioButtons(x => x.setAttribute("onchange", "onRadio()"));
 onRadio();
+
+function getItemString(id, name, description, price) {
+	return `<label class="list-group-item list-group-item-action d-flex lh-condensed">
+			<input type="checkbox" class="big-checkbox" name="${id}">
+			<div class="d-flex justify-content-between flex-fill pl-3">
+				<div>
+					<h6 class="my-0">${name}</h6>
+					<small class="text-muted">${description}</small>
+				</div>
+				<span class="text-muted">$${price}</span>
+			</div>
+		</label>`;
+}
+
+function getTotalString(total) {
+	return `<li class="list-group-item d-flex justify-content-between">
+			<span>Total (USD)</span>
+			<strong>$${total}</strong>
+		</li>`;
+}
+
+document.getElementById("loaded-items").innerHTML = getItemString(1, "Salad", "Crisp Vegetables", 3) + getTotalString(20);
