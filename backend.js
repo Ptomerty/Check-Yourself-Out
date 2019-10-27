27e4e0012c45a2f668473d4c841f5e01435ea273
Ptomerty <Ptomerty@users.noreply.github.com>
2019-10-27 00:43:50 -0400

    fix: accept all domains

--- backend.js
+++ backend.js
@@ -122,7 +122,7 @@ async function main() {
 	app.use(express.json());
 	app.use(cors());
 	app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+	  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	  next();
 	});
