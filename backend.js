61d21f3f1116588eebd410f35884d83c816001ea
Ptomerty <Ptomerty@users.noreply.github.com>
2019-10-27 00:42:14 -0400

    fix: fix copy

--- backend.js
+++ backend.js
@@ -122,8 +122,8 @@ async function main() {
 	app.use(express.json());
 	app.use(cors());
 	app.use(function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content$
+	  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	  next();
 	});
 
